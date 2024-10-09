106bc9c934028cca5c3798199c0cb1281711ea06
DeyangLiu123 <162725892+DeyangLiu123@users.noreply.github.com>
2024-10-09 17:01:40 +0800

    Update pthread.c

--- pthread.c
+++ pthread.c
@@ -107,7 +107,8 @@ void configureTerminal() {
 int kbhit(void) {
     struct termios oldt, newt;
     int oldf;
-
+    int ch；
+    
     tcgetattr(STDIN_FILENO, &oldt);
     newt = oldt;
     newt.c_lflag &= ~(ICANON | ECHO);
@@ -115,7 +116,7 @@ int kbhit(void) {
     oldf = fcntl(STDIN_FILENO, F_GETFL, 0);
     fcntl(STDIN_FILENO, F_SETFL, oldf | O_NONBLOCK);
 
-    int ch = getchar();
+    ch = getchar();
 
     tcsetattr(STDIN_FILENO, TCSANOW, &oldt);
     fcntl(STDIN_FILENO, F_SETFL, oldf);
